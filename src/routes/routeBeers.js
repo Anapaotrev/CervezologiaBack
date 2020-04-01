cc77bbf7f280a13bbf9cf8a75a1cc4434fad28c0
Eduardo Alejandro Toraya Solís <eduar_ts1997@hotmail.com>
2020-04-01 15:39:23 -0600

    Routes for beers and places

---
+++ src/routes/routeBeers.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router = express.Router()
+
+const beers = require("../controllers/beers")
+
+router.get("/beer", beers.getBeer)
+router.get("/beers", beers.getBeers)
+router.post("/beer", beers.newBeer)
+router.patch("/beer", beers.updateBeer)
+router.detele("/beer", beers.deleteBeer)
---
+++ src/routes/routePlaces.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router = express.Router()
+
+const places = require("../controllers/places")
+
+router.get("/place", beers.getPlace)
+router.get("/places", beers.getPlaces)
+router.post("/place", beers.newPlace)
+router.patch("/place", beers.updatePlace)
+router.detele("/place", beers.deletePlace)
