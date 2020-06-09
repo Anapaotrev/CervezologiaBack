b0b3a2d4712fd43f9600425d666365b1c24c5282
Diego Martinez <diegomtz1998@hotmail.com>
2020-06-09 12:19:53 -0500

    Add admin page

--- src/app.js
+++ src/app.js
@@ -1,15 +1,45 @@
 const express = require('express')
+const bcryptjs = require('bcryptjs')
 require('./db/db')
 var cors = require('cors');
+const AdminBro = require('admin-bro')
+const AdminBroExpressjs = require('admin-bro-expressjs')
 
 const userRoutes = require('./routes/user')
 const placeRoutes = require('./routes/places')
 const beerRoutes = require('./routes/beers')
 const diaryRoutes = require('./routes/diaries')
 
+const User = require('./models/user')
+const Beer = require('./models/beer')
+const Diary = require('./models/diary')
+const Place = require('./models/place')
+
 const app = express()
 const port = process.env.PORT || 3000
 
+AdminBro.registerAdapter(require('admin-bro-mongoose'))
+
+const adminBro = new AdminBro({
+  resources: [User, Beer, Diary, Place],
+  rootPath: '/admin',
+})
+
+const router = AdminBroExpressjs.buildAuthenticatedRouter(adminBro, {
+  authenticate: async (email, password) => {
+    const user = await User.findOne({ email })
+    if (user) {
+      const matched = await bcryptjs.compare(password, user.password)
+      if (matched) {
+        return user
+      }
+    }
+    return false
+  },
+  cookiePassword: 'secret',
+})
+
+app.use(adminBro.options.rootPath, router)
 app.use(cors())
 app.use(express.json())
 app.use('/', userRoutes, placeRoutes, beerRoutes, diaryRoutes);
