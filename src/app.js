740dee36fb612b5f82040c32e6f60c50d7ae0a39
hectordeluna <hectordelunap@gmail.com>
2020-06-10 08:35:12 -0500

    change base api url

--- src/app.js
+++ src/app.js
@@ -1,53 +1,54 @@
-const express = require('express')
-const bcryptjs = require('bcryptjs')
-require('./db/db')
-var cors = require('cors');
-const AdminBro = require('admin-bro')
-const AdminBroExpressjs = require('admin-bro-expressjs')
+const express = require("express");
+const bcryptjs = require("bcryptjs");
+require("./db/db");
+var cors = require("cors");
+const AdminBro = require("admin-bro");
+const AdminBroExpressjs = require("admin-bro-expressjs");
 
-const userRoutes = require('./routes/user')
-const placeRoutes = require('./routes/places')
-const beerRoutes = require('./routes/beers')
-const diaryRoutes = require('./routes/diaries')
+const userRoutes = require("./routes/user");
+const placeRoutes = require("./routes/places");
+const beerRoutes = require("./routes/beers");
+const diaryRoutes = require("./routes/diaries");
 
-const User = require('./models/user')
-const Beer = require('./models/beer')
-const Diary = require('./models/diary')
-const Place = require('./models/place')
+const User = require("./models/user");
+const Beer = require("./models/beer");
+const Diary = require("./models/diary");
+const Place = require("./models/place");
 
-const app = express()
-const port = process.env.PORT || 3000
+const app = express();
+const port = process.env.PORT || 3000;
 
-AdminBro.registerAdapter(require('admin-bro-mongoose'))
+AdminBro.registerAdapter(require("admin-bro-mongoose"));
 
 const adminBro = new AdminBro({
   resources: [User, Beer, Diary, Place],
-  rootPath: '/admin',
+  rootPath: "/admin",
   branding: {
-    companyName: 'Cervezología',
-    softwareBrothers: false
+    companyName: "Cervezología",
+    softwareBrothers: false,
   },
-})
+});
 
 const router = AdminBroExpressjs.buildAuthenticatedRouter(adminBro, {
   authenticate: async (email, password) => {
-    const user = await User.findOne({ email })
-    if (user.role === 'admin') {
-      const matched = await bcryptjs.compare(password, user.password)
+    const user = await User.findOne({ email });
+    if (user.role === "admin") {
+      const matched = await bcryptjs.compare(password, user.password);
       if (matched) {
-        return user
+        return user;
       }
     }
-    return false
+    return false;
   },
-  cookiePassword: 'secret',
-})
+  cookiePassword: "secret",
+});
 
-app.use(adminBro.options.rootPath, router)
-app.use(cors())
-app.use(express.json())
-app.use('/', userRoutes, placeRoutes, beerRoutes, diaryRoutes);
+app.use(adminBro.options.rootPath, router);
+app.use(cors());
+app.use(express.json());
+app.use("/api/", userRoutes, placeRoutes, beerRoutes, diaryRoutes);
 
-app.listen(port, function() {
-  console.log('Server up and running on port ' + port)
-})
+app.listen(port, function () {
+  console.log("Server up and running on port " + port);
+});
+
